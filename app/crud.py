98388c496b58a97b7f249cad51b63ca338b05bbe
PauloS5 <paulocratesdesouza@gmail.com>
2025-09-16 16:30:54 -0500

    assinatura das funções de crud.py criadas

---
+++ app/crud.py
@@ -0,0 +1,19 @@
+from sqlmodel import Session
+
+def all(session: Session) -> any:
+    return
+
+def find(session: Session, id: int) -> any:
+    return
+
+def findByUrl(session: Session, url: str) -> any:
+    return
+
+def save(session: Session, url: str) -> any:
+    return
+
+def update(session: Session, id: int, newurl: str) -> any:
+    return
+
+def delete(session: Session, id: int):
+    return
