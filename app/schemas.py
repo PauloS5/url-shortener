97234c0fe997dbc442af9de1371bc8fecc09783d
PauloS5 <paulocratesdesouza@gmail.com>
2025-09-16 16:22:24 -0500

    atributos dos schemas de Url implementados

--- app/schemas.py
+++ app/schemas.py
@@ -1,17 +1,29 @@
 from sqlmodel import SQLModel, Field
 
 class UrlBase(SQLModel):
-    original_url: str = Field(max_length=511)
+    original_url: str = Field(
+        title="URL original",
+        description="URL na quala API redirecionará.",
+        max_length=511)
 
 class UrlCreate(UrlBase):
     pass
 
 class UrlRead(UrlBase):
-    id: int = Field()
-    nick_url: str = Field(default=None)
+    id: int = Field(
+        title="ID",
+        description="Indenttificador único auto-incrementável")
+    nick_url: str = Field(
+        default=None,
+        title="URL de apelido",
+        description="End-point que apontará para a URL relacionada.")
 
 class UrlUpdate(UrlBase):
-    id: int
+    id: int = Field(
+        title="ID",
+        description="Identificador único que aponta para o registro a ser atualizado.")
 
 class UrlDelete(UrlBase):
-    id: int = Field()
+    id: int = Field(
+        title="ID",
+        description="Identificador único que aponta para o registro a ser excluído.")
