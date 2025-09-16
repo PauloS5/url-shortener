c0639740a464a33b9587ef3da8e8cd43aa12ad0d
PauloS5 <paulocratesdesouza@gmail.com>
2025-09-16 16:08:31 -0500

    cabeçalho das classes criados

---
+++ app/schemas.py
@@ -0,0 +1,16 @@
+from sqlmodel import SQLModel, Field
+
+class UrlBase(SQLModel):
+    pass
+
+class UrlCreate(SQLModel):
+    pass
+
+class UrlRead(SQLModel):
+    pass
+
+class UrlUpdate(SQLModel):
+    pass
+
+class UrlDelete(SQLModel):
+    pass
