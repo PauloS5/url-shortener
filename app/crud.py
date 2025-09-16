c9767c5e83417fcf5b5809e2a2d8a18fa5727c2a
PauloS5 <paulocratesdesouza@gmail.com>
2025-09-16 17:13:07 -0500

    type hints das funções atualizados

--- app/crud.py
+++ app/crud.py
@@ -1,22 +1,22 @@
 from sqlmodel import Session
 from .models import Url
 
-def all(session: Session) -> any:
+def all(session: Session) -> list[Url]:
     return
 
-def find(session: Session, id: int) -> any:
+def find(session: Session, id: int) -> Url:
     return
 
-def findByUrl(session: Session, url: str) -> any:
+def findByUrl(session: Session, url: str) -> Url:
     return
 
-def save(session: Session, url: str, nickurl) -> any:
+def save(session: Session, url: str, nickurl) -> None:
     new = Url(original_url=url, nick_url=nickurl)
     session.add(new)
     session.commit()
 
-def update(session: Session, id: int, newurl: str) -> any:
+def update(session: Session, id: int, newurl: str) -> None:
     return
 
-def delete(session: Session, id: int):
+def delete(session: Session, id: int) -> None:
     return
