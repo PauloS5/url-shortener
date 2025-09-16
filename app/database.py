e75878c43622f499c7a97135570df78dc5f01e8a
PauloS5 <paulocratesdesouza@gmail.com>
2025-09-16 16:53:00 -0500

    funções para conexão com o banco criadas

--- app/database.py
+++ app/database.py
@@ -1,4 +1,4 @@
-from sqlmodel import Session, create_engine
+from sqlmodel import SQLModel, Session, create_engine
 
 DB_DRIVER = "mysql-pymysql"
 DB_USER = "root"
@@ -8,3 +8,15 @@ DB_PORT = "3306"
 DB_NAME = "dbUrlShortener"
 
 DB_URL = f"{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DB_URL, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+if __name__ == "__main__":
+    create_db_and_tables()
