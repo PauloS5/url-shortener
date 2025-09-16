246558acee65968239971623b7d2ff68272fa099
PauloS5 <paulocratesdesouza@gmail.com>
2025-09-16 17:49:17 -0500

    find() implementado - troca de session por engine realizada

--- app/crud.py
+++ app/crud.py
@@ -1,24 +1,32 @@
 from sqlmodel import Session, select
 from .models import Url
 
-def all(session: Session) -> list[Url]:
-    statement = select(Url)
-    results = session.exec(statement)
-    return results.all()
+def all(engine) -> list[Url]:
+    with Session(engine) as session:
+        statement = select(Url)
+        results = session.exec(statement)
+        return results.all()
 
-def find(session: Session, id: int) -> Url:
-    return
+def find(engine, id: int) -> Url:
+    with Session(engine) as session:
+        statement = select(Url).where(Url.id == id).limit(1)
+        results = session.exec(statement)
+        return results.first()
 
-def findByUrl(session: Session, url: str) -> Url:
-    return
+def findByUrl(engine, url: str) -> Url:
+    with Session(engine) as session:
+        return
 
-def save(session: Session, url: str, nickurl) -> None:
-    newurl = Url(original_url=url, nick_url=nickurl)
-    session.add(newurl)
-    session.commit()
+def save(engine, url: str, nickurl: str) -> None:
+    with Session(engine) as session:
+        newurl = Url(original_url=url, nick_url=nickurl)
+        session.add(newurl)
+        session.commit()
 
-def update(session: Session, id: int, newurl: str) -> None:
-    return
+def update(engine, id: int, newurl: str) -> None:
+    with Session(engine) as session:
+        return
 
-def delete(session: Session, id: int) -> None:
-    return
+def delete(engine, id: int) -> None:
+    with Session(engine) as session:
+        return
