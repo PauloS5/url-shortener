11030b4ed8d95c269535624e692b42c6405198e9
PauloS5 <paulocratesdesouza@gmail.com>
2025-09-18 17:56:12 -0500

    rota get:/url implementada

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,6 @@ from .schemas import UrlRead
 
 router = APIRouter()
 
-@router.get("/url", response_model=list[UrlRead])
-async def all_url(session: Session = Depends(get_session)) -> any:
+@router.get("/url/", response_model=list[UrlRead])
+async def list_urls(session: Session = Depends(get_session)) -> any:
     return all(session)
