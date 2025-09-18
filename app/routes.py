7948c1f1d62301303b864366b4c6bbbd2553253e
PauloS5 <paulocratesdesouza@gmail.com>
2025-09-18 18:04:39 -0500

    rota get:url/id criada

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Path, Depends
 from sqlmodel import Session
 
 from .database import get_session
-from .crud import all
+from .crud import *
 from .schemas import UrlRead 
 
 router = APIRouter()
@@ -10,3 +10,10 @@ router = APIRouter()
 @router.get("/url/", response_model=list[UrlRead])
 async def list_urls(session: Session = Depends(get_session)) -> any:
     return all(session)
+
+@router.get("/url/{id}", response_model=UrlRead|None)
+async def get_url(session: Session = Depends(get_session), id: int = Path(title="Identificador Único", description="ID que seráusado para buscar uma url")):
+    url = find(session, id)
+
+    return url
+
