fde409c27cdc27a1a20a9691d5e03bacebf314be
PauloS5 <paulocratesdesouza@gmail.com>
2025-09-19 16:47:46 -0500

    o gerador de endpoints não gera mais com caracteres de ponto

--- app/helpers.py
+++ app/helpers.py
@@ -3,7 +3,7 @@ import random
 
 def generate_random_string():
     TAM = 10
-    chars = string.ascii_letters + string.digits + string.punctuation + "-_"
+    chars = string.ascii_letters + string.digits + "-_"
     random_string = ''.join(random.choice(chars) for _ in range(TAM))
 
     return random_string
