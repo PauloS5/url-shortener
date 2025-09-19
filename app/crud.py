1714b2589daee0c897d01b2f4df991af9a2f85cb
PauloS5 <paulocratesdesouza@gmail.com>
2025-09-19 18:00:18 -0500

    funções renomeadas

--- app/crud.py
+++ app/crud.py
@@ -1,27 +1,27 @@
 from sqlmodel import Session, select
 from .models import Url
 
-def all(session: Session) -> list[Url]:
+def all_urls(session: Session) -> list[Url]:
     statement = select(Url)
     results = session.exec(statement)
     return results.all()
 
-def find(session: Session, id: int) -> Url:
+def find_url(session: Session, id: int) -> Url:
     statement = select(Url).where(Url.id == id).limit(1)
     results = session.exec(statement)
     return results.first()
 
-def findByNick(session: Session, nick: str) -> Url:
+def find_url_by_nick(session: Session, nick: str) -> Url:
     statement = select(Url).where(Url.nick_url == nick).limit(1)
     results = session.exec(statement)
     return results.first()
 
-def save(session: Session, newurl: str, nickurl: str) -> None:
+def save_url(session: Session, newurl: str, nickurl: str) -> None:
     newurl = Url(url=newurl, nick_url=nickurl)
     session.add(newurl)
     session.commit()
 
-def update(session: Session, id: int, newurl: str) -> None:
+def update_url(session: Session, id: int, newurl: str) -> None:
     statement = select(Url).where(Url.id == id)
     result = session.exec(statement)
     url = result.first()
@@ -31,7 +31,7 @@ def update(session: Session, id: int, newurl: str) -> None:
         session.add(url)
         session.commit()
 
-def delete(session: Session, id: int) -> None:
+def delete_url(session: Session, id: int) -> None:
     statement = select(Url).where(Url.id == id)
     result = session.exec(statement)
     url = result.first()
--- app/routes.py
+++ app/routes.py
@@ -9,26 +9,26 @@ from .helpers import *
 router = APIRouter()
 
 @router.get("/url/", response_model=list[UrlRead])
-async def list_urls(session: Session = Depends(get_session)) -> any:
-    return all(session)
+async def list_all_urls(session: Session = Depends(get_session)) -> any:
+    return all_urls(session)
 
 @router.get("/url/{id}", response_model=UrlRead|None)
 async def get_url(session: Session = Depends(get_session), id: int = Path(title="Identificador Único", description="ID que seráusado para buscar uma url")) -> any:
-    return find(session, id)
+    return find_url(session, id)
 
 @router.post("/url/", response_model=None)
 async def register_url(session: Session = Depends(get_session), url_to_register: UrlCreate = Body(embed=True, title="URL", description="URL que será cadastrada no banco de dados")):
     endpoint_nickurl = generate_random_string()
 
-    while findByNick(session, endpoint_nickurl) != None:
+    while find_url_by_nick(session, endpoint_nickurl) != None:
         endpoint_nickurl = generate_random_string()
         
-    save(session, newurl=url_to_register.url, nickurl=endpoint_nickurl)
+    save_url(session, newurl=url_to_register.url, nickurl=endpoint_nickurl)
 
 @router.put("/url/", response_model=None)
 async def update_url(session: Session = Depends(get_session), url_to_update: UrlUpdate = Body(embed=True, title="Nova URL", description="URL que substituirá a antiga")) -> any:
-    update(session, url_to_update.id, url_to_update.url)
+    update_url(session, url_to_update.id, url_to_update.url)
 
 @router.delete("/url/{id}", response_model=None)    
 async def delete_url(session: Session = Depends(get_session), id: int = Path(title="Identificador Único", description="ID do elemento que será excluído")) -> any:
-    delete(session, id)
+    delete_url(session, id)
